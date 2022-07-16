98af3ee86da69a2d0de5286451854d0c37b3e5cd
Winrida <winrida@yandex.ru>
2022-07-16 13:08:40 +0300

    Second commit.

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -92,7 +92,7 @@ public class Main {
         } else {
             try {
                 if (result > 0) {
-                    return toRoman(result);
+                    return Roman.toRoman(result);
                 } else {
                     throw new Exception();
                 }
@@ -103,46 +103,5 @@ public class Main {
             }
         }
         return null;
-    }
-
-    public static String toRoman(int input) {
-        String number = "";
-        while (input >= 100) {
-            number += "C";
-            input -= 100;
-        }
-        while (input >= 90) {
-            number += "XC";
-            input -= 90;
-        }
-        while (input >= 50) {
-            number += "L";
-            input -= 50;
-        }
-        while (input >= 40) {
-            number += "XL";
-            input -= 40;
-        }
-        while (input >= 10) {
-            number += "X";
-            input -= 10;
-        }
-        while (input >= 9) {
-            number += "IX";
-            input -= 9;
-        }
-        while (input >= 5) {
-            number += "V";
-            input -= 5;
-        }
-        while (input >= 4) {
-            number += "IV";
-            input -= 4;
-        }
-        while (input >= 1) {
-            number += "I";
-            input -= 1;
-        }
-        return number;
     }
 }
--- src/calculator/Roman.java
+++ src/calculator/Roman.java
@@ -12,4 +12,45 @@ public enum Roman {
     public int getNum() {
         return num;
     }
+
+    public static String toRoman(int input) {
+        String number = "";
+        if (input >= 100) {
+            number += "C";
+            input -= 100;
+        }
+        if (input >= 90) {
+            number += "XC";
+            input -= 90;
+        }
+        while (input >= 50) {
+            number += "L";
+            input -= 50;
+        }
+        while (input >= 40) {
+            number += "XL";
+            input -= 40;
+        }
+        while (input >= 10) {
+            number += "X";
+            input -= 10;
+        }
+        while (input >= 9) {
+            number += "IX";
+            input -= 9;
+        }
+        while (input >= 5) {
+            number += "V";
+            input -= 5;
+        }
+        while (input >= 4) {
+            number += "IV";
+            input -= 4;
+        }
+        while (input >= 1) {
+            number += "I";
+            input -= 1;
+        }
+        return number;
+    }
 }
