8bd9cbdeee150ae5b8c33ee40ebffaa1213cdf44
Winrida <winrida@yandex.ru>
2022-07-17 02:29:10 +0300

    Third commit.

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -4,65 +4,53 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println(calc(scanner.nextLine()));
         scanner.close();
     }
 
-    public static String calc(String input) {
+    public static String calc(String input) throws Exception {
         String[] parts = input.split(" ");
 
-        if(parts.length != 3) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("throws Exception");
-                System.exit(0);
-            }
+        if (parts.length != 3) {
+            throw new Exception();
         }
         return calculate(parts);
     }
 
-    public static String calculate(String[] expression) {
+    public static String calculate(String[] expression) throws Exception {
         boolean isRoman1 = false;
         boolean isRoman2 = false;
         boolean areRoman = false;
         int result;
-        try{
+        try {
             if (Integer.parseInt(expression[0]) <= 0 || Integer.parseInt(expression[0]) > 10 ||
                     Integer.parseInt(expression[2]) <= 0 || Integer.parseInt(expression[2]) > 10){
                 throw new Exception();
             }
         }
         catch (Exception e){
-            try{
-                for (Roman roman : Roman.values()){
-                    if (!isRoman1 && roman.toString().equals(expression[0])){
-                        isRoman1 = true;
-                        expression[0] = Integer.toString(roman.getNum());
-                    }
-                    if (!isRoman2 && roman.toString().equals(expression[2])){
-                        isRoman2 = true;
-                        expression[2] = Integer.toString(roman.getNum());
-                    }
-                    if (isRoman1 && isRoman2){
-                        break;
-                    }
+            for (Roman roman : Roman.values()){
+                if (!isRoman1 && roman.toString().equals(expression[0])){
+                    isRoman1 = true;
+                    expression[0] = Integer.toString(roman.getNum());
                 }
-                if (isRoman1 != isRoman2) {
-                    throw new Exception();
-                } else if (isRoman1 && isRoman2) {
-                    areRoman = true;
-                } else {
-                    throw new Exception();
+                if (!isRoman2 && roman.toString().equals(expression[2])){
+                    isRoman2 = true;
+                    expression[2] = Integer.toString(roman.getNum());
+                }
+                if (isRoman1 && isRoman2){
+                    break;
                 }
             }
-            catch (Exception exp){
-                System.out.println("throws Exception");
-                System.exit(0);
+            if (isRoman1 != isRoman2) {
+                throw new Exception();
+            } else if (isRoman1 && isRoman2) {
+                areRoman = true;
+            } else {
+                throw new Exception();
             }
-
         }
 
         switch (expression[1]) {
@@ -79,29 +67,16 @@ public class Main {
                 result = Integer.parseInt(expression[0]) / Integer.parseInt(expression[2]);
                 break;
             default:
-                try {
-                    throw new Exception();
-                } catch (Exception e) {
-                    System.out.println("throws Exception");
-                    System.exit(0);
-                    return "";
-                }
+                throw new Exception();
         }
         if (!areRoman) {
             return Integer.toString(result);
         } else {
-            try {
-                if (result > 0) {
-                    return Roman.toRoman(result);
-                } else {
-                    throw new Exception();
-                }
-            }
-            catch (Exception e){
-                System.out.println("throws Exception");
-                System.exit(0);
+            if (result > 0) {
+                return Roman.toRoman(result);
+            } else {
+                throw new Exception();
             }
         }
-        return null;
     }
 }
